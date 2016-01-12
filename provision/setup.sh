6d06d35bac7001ab0efe86174acc37612ecac8b2
Anton Ohorodnyk <anton@ohorodnyk.name>
2016-01-12 14:58:12 +0200

    Added composer and mysql installation

--- provision/setup.sh
+++ provision/setup.sh
@@ -17,10 +17,14 @@ echo "Installing PHP 7"
 sudo apt-get purge php5-fpm -y && apt-get --purge autoremove -y
 sudo add-apt-repository ppa:ondrej/php-7.0
 sudo apt-get update
-sudo apt-get install php-xdebug php7.0-fpm php7.0-curl php7.0-cli php7.0-mcrypt php7.0-json php7.0-sqlite3 -y
+sudo apt-get install php-xdebug php7.0-fpm php7.0-curl php7.0-cli php7.0-mcrypt php7.0-json php7.0-sqlite3 php7.0-intl php7.0-xsl php7.0-gd php7.0-mysql -y
 sudo sed -i "s/www-data/vagrant/g" /etc/php/7.0/fpm/pool.d/www.conf
 sudo sed -i "s/;listen.mode/listen.mode/g" /etc/php/7.0/fpm/pool.d/www.conf
 sudo service php7.0-fpm restart
+
+echo "Installing composer"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
 
 echo "Installing Nginx"
 sudo echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/sources.list
@@ -36,5 +40,10 @@ sudo service nginx restart
 echo "Preparing infrastructure"
 sudo mkdir -p /var/www/ohorodnyk.dev
 sudo chown -R vagrant.vagrant /var/www
+
+echo "Installing MySQL"
+echo "mysql-server-5.6 mysql-server/root_password password 123123q" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password 123123q" | sudo debconf-set-selections
+sudo apt-get install mysql-server-5.6 mysql-client-5.6 -y
 
 echo "Finished provisioning."
