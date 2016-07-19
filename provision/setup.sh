8ea9ee88556f9bc18adc5cd32978e1caaecc8021
Anton Ohorodnyk <anton@ohorodnyk.name>
2016-07-19 14:37:49 +0300

    Fixed small issues

--- provision/setup.sh
+++ provision/setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo apt-get install -y language-pack-en
 sudo locale-gen "en_US.UTF-8"
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker ubuntu
 
 wget -nv -O docker-compose https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m`
 sudo mv docker-compose /usr/local/bin/docker-compose
@@ -23,7 +23,7 @@ echo "Installing PHP 7"
 sudo apt-get install -y php-fpm php-curl php-cli php-mcrypt php-json php-xdebug \
     php-sqlite3 php-intl php-xsl php-gd php-mysql php-amqp php-amqplib php-zip
 sudo sed -i "s/www-data/ubuntu/g" /etc/php/7.0/fpm/pool.d/www.conf
-sudo sed -i "s/;listen.mode/listen.mode/g" /etc/php/7.0/fpm/pool.d/www.conf
+sudo sed -i "s/;listen.mode/listen.mode = 0777;/g" /etc/php/7.0/fpm/pool.d/www.conf
 sudo service php7.0-fpm restart
 
 echo "Installing composer"
